3ac2e918f366b0ebb04a4f2c9ee94abee43032ba
RedSponge <eran.godsi2003@gmail.com>
2020-07-07 20:11:28 +0300

    Create green bg test

--- core/src/main/java/com/redsponge/sponge/SpongeGame.java
+++ core/src/main/java/com/redsponge/sponge/SpongeGame.java
@@ -1,7 +1,16 @@
 package com.redsponge.sponge;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class SpongeGame extends ApplicationAdapter {
+
+    @Override
+    public void render() {
+        super.render();
+        Gdx.gl.glClearColor(0, 1, 0, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
 }
