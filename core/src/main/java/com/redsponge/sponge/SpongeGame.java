8d81db8a779545f9e79df2da4caa2a40d9fc1127
RedSponge <eran.godsi2003@gmail.com>
2020-07-07 20:08:36 +0300

    Initial commit

---
+++ core/src/main/java/com/redsponge/sponge/SpongeGame.java
@@ -0,0 +1,7 @@
+package com.redsponge.sponge;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class SpongeGame extends ApplicationAdapter {
+}
