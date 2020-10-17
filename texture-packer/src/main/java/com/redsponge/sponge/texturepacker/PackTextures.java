b50da74ba26f00e2778b9efea4cbb09343f35631
RedSponge <eran.godsi2003@gmail.com>
2020-10-17 22:28:57 +0300

    Fix texturepacker project

---
+++ texture-packer/src/main/java/com/redsponge/sponge/texturepacker/PackTextures.java
@@ -0,0 +1,11 @@
+package com.redsponge.sponge.texturepacker;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class PackTextures {
+
+    public static void main(String[] args) {
+
+    }
+
+}
