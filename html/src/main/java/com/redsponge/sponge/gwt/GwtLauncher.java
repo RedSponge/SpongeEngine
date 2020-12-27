86c75ea00979be4dea80bc79842de21e3afef8cd
Eran <eran.godsi2003@gmail.com>
2020-12-28 00:25:41 +0200

    Change launcher

--- html/src/main/java/com/redsponge/sponge/gwt/GwtLauncher.java
+++ html/src/main/java/com/redsponge/sponge/gwt/GwtLauncher.java
@@ -4,9 +4,13 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Panel;
 import com.redsponge.sponge.SpongeGame;
 
 /** Launches the GWT application. */
@@ -20,36 +24,49 @@ public class GwtLauncher extends GwtApplication {
 
 		////UNCOMMENT THIS CODE FOR A RESIZABLE APPLICATION
 		//	PADDING is to avoid scrolling in iframes, set to 20 if you have problems
-			private static final int PADDING = 0;
 
-			@Override
-			public GwtApplicationConfiguration getConfig() {
-				int w = Window.getClientWidth() - PADDING;
-				int h = Window.getClientHeight() - PADDING;
-				GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(w, h);
-				Window.enableScrolling(false);
-				Window.setMargin("0");
-				Window.addResizeHandler(new ResizeListener());
-				return cfg;
-			}
+	private static final int PADDING = 0;
 
-			class ResizeListener implements ResizeHandler {
-				@Override
-				public void onResize(ResizeEvent event) {
-                  if (Gdx.graphics.isFullscreen()) return;
-					int width = event.getWidth() - PADDING;
-					int height = event.getHeight() - PADDING;
-					getRootPanel().setWidth("" + width + "px");
-					getRootPanel().setHeight("" + height + "px");
-					getApplicationListener().resize(width, height);
-					Gdx.graphics.setWindowedMode(width, height);
-				}
-			}
-		////END OF CODE FOR RESIZABLE APPLICATION
+	@Override
+	public GwtApplicationConfiguration getConfig() {
+		int w = Window.getClientWidth() - PADDING;
+		int h = Window.getClientHeight() - PADDING;
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(w, h);
+		Window.enableScrolling(false);
+		Window.setMargin("0");
+		Window.addResizeHandler(new ResizeListener());
+		return cfg;
+	}
 
+	class ResizeListener implements ResizeHandler {
 		@Override
-		public ApplicationListener createApplicationListener () { 
-			return new SpongeGame();
+		public void onResize(ResizeEvent event) {
+			if (Gdx.graphics.isFullscreen()) return;
+			int width = event.getWidth() - PADDING;
+			int height = event.getHeight() - PADDING;
+			getRootPanel().setWidth("" + width + "px");
+			getRootPanel().setHeight("" + height + "px");
+			getApplicationListener().resize(width, height);
+			Gdx.graphics.setWindowedMode(width, height);
 		}
+	}
 
+	@Override
+	public Preloader.PreloaderCallback getPreloaderCallback() {
+		return createPreloaderPanel(GWT.getHostPageBaseURL() + "preloadlogo.png");
+	}
+
+	@Override
+	protected void adjustMeterPanel(Panel meterPanel, Style meterStyle) {
+		meterPanel.setStyleName("gdx-meter");
+		meterPanel.addStyleName("nostripes");
+		meterStyle.setProperty("backgroundColor", "#ffbb00");
+		meterStyle.setProperty("backgroundImage", "none");
+	}
+	////END OF CODE FOR RESIZABLE APPLICATION
+
+	@Override
+	public ApplicationListener createApplicationListener () {
+		return new SpongeGame();
+	}
 }
